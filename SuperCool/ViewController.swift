906aeaeb9554638d507012d0324d24ef97c355dc
user116176 <user116176@77-240-12-7.rdns.melbourne.co.uk>
2016-01-18 08:42:44 +0000

    Initial Commit

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by user116176 on 1/18/16.
+//  Copyright © 2016 Steve Light. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
